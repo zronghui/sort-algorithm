7cb5fb055c9f830c1f4ba9f1fa125ca5011896d4
zronghui <825503975@qq.com>
2020-06-08 16:19:37 +0800

    更新部分代码

--- answers/6.quickSort.py
+++ answers/6.quickSort.py
@@ -14,27 +14,26 @@ def partition(a, left, right):
     # 可以记录比 pivot 小的数的最大索引，然后和 pivot 交换
     # 举例：[3, 2, 5, 1, 6]--> [3, 2, 1, 5, 6] --> [1, 2, 3, 5, 6]
     # 首先分成 2 1 和 5 6，比 pivot 小的最大位置是 1 ，和 3 交换即可，最后返回 3 的索引
-    pivot = left
-    # i 用来遍历，index-1 标记比 pivot 小的数的最大位置
-    i = index = left + 1
-    while i <= right:
-        if a[i] < a[pivot]:
-            swap(a, i, index)
-            index += 1
-        i += 1
-    swap(a, index - 1, pivot)
-    return index - 1
+    # p2 用来遍历，p1-1 标记比 pivot 小的数的最大位置
+    pivot = a[left]
+    p1 = p2 = left + 1
+    while p2 <= right:
+        if a[p2] < pivot:
+            swap(a, p1, p2)
+            p1 += 1
+        p2 += 1
+    swap(a, p1 - 1, left)
+    return p1 - 1
 
 
 def quickSort(a, left=None, right=None):
-    n = len(a)
     left = 0 if left is None else left
-    right = n - 1 if right is None else right
-    if n < 2 or right - left < 1:
-        return
-    p = partition(a, left, right)
-    quickSort(a, left, p - 1)
-    quickSort(a, p + 1, right)
+    right = len(a) - 1 if right is None else right
+    if right - left < 1:
+        return a
+    pivot = partition(a, left, right)
+    quickSort(a, left, pivot - 1)
+    quickSort(a, pivot + 1, right)
 
 
 if __name__ == '__main__':
